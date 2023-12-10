cd30074f4807089e00994a0e99d0b985f5acbbca
KhadijaRG <kgh@camas-global.com>
2023-12-10 20:39:35 +0100

    article page integration

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.blog');
 });
-
+Route::get('/article', function () {
+    return view('pages.article');
+});
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
